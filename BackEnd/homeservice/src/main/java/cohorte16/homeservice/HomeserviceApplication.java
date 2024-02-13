1874f847fbd026129f9185c750d024ecf2a949c3
Franco Tallei <ftallei.work@gmail.com>
2024-02-13 10:58:48 -0300

    Proyecto SpringBoot y Estructura de carpetas

---
+++ BackEnd/homeservice/src/main/java/cohorte16/homeservice/HomeserviceApplication.java
@@ -0,0 +1,13 @@
+package cohorte16.homeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeserviceApplication.class, args);
+	}
+
+}
