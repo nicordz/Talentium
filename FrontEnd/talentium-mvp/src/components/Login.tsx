186f92776c06084be69291f23812a286ba9f5083
Angel-Alvarez <144290231+angealvarez1211@users.noreply.github.com>
2024-02-15 10:58:35 -0300

    "agregando Header y Login"

---
+++ FrontEnd/talentium-mvp/src/components/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <form action="">
+        <h2>Login</h2>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="contraseña" />
+        <button type="submit">Entrar</button>
+    </form>
+  )
+}
+
+export default Login
