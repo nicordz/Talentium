e55d4329f55575c44bbbec1dad01163ae3ecdd53
Angel-Alvarez <144290231+angealvarez1211@users.noreply.github.com>
2024-02-15 13:20:25 -0300

    Arreglo de App

--- FrontEnd/talentium-mvp/src/App.tsx
+++ FrontEnd/talentium-mvp/src/App.tsx
@@ -1,57 +1,43 @@
-<<<<<<< HEAD
+import React, { useState } from "react";
+import RegisterForm from "./components/RegisterForm";
+import Footer from "./components/common/Footer";
+import UserTypeSelector from "./components/UserTypeSelector";
+import { UserType } from "./interfaces/RegisterFormTypes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
 
-import './App.css'
-import Header from './components/Header';
-import Login from './components/Login';
+const App: React.FC = () => {
+  const [userType, setUserType] = useState<UserType | null>(null);
 
-function App() {
+  const handleSelectedUserType = (SelectedUserType: UserType) => {
+    setUserType(SelectedUserType);
+  };
 
   return (
     <>
       <Header />
       <Login />
+      <div className="flex bg-gradient-to-b from-[#4069E4] to-white h-screen justify-center items-center p-4">
+        <ToastContainer />
+        {userType === null ? (
+          <UserTypeSelector onSelectedUserType={handleSelectedUserType} />
+        ) : (
+          <RegisterForm
+            userType={userType}
+            name=""
+            surname=""
+            mail=""
+            password=""
+          />
+        )}
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
-  )
-}
-=======
-import React, { useState } from 'react';import RegisterForm from './components/RegisterForm';
-import Footer from './components/common/Footer';
-import UserTypeSelector from './components/UserTypeSelector';
-import { UserType } from './interfaces/RegisterFormTypes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-const App: React.FC = () => {
-    const [userType, setUserType] = useState<UserType | null>(null);
-
-    const handleSelectedUserType = (SelectedUserType: UserType) => {
-        setUserType(SelectedUserType);
-    };
->>>>>>> 820b4f355d64bebb65e34c965bfd60d36e67addb
-
-    return (
-        <div>
-            <div className='flex bg-gradient-to-b from-[#4069E4] to-white h-screen justify-center items-center p-4'>
-                <ToastContainer />
-                {userType === null ? (
-                    <UserTypeSelector
-                        onSelectedUserType={handleSelectedUserType}
-                    />
-                ) : (
-                    <RegisterForm
-                        userType={userType}
-                        name=''
-                        surname=''
-                        mail=''
-                        password=''
-                    />
-                )}
-            </div>
-            <div>
-                <Footer />
-            </div>
-        </div>
-    );
+  );
 };
 
 export default App;
