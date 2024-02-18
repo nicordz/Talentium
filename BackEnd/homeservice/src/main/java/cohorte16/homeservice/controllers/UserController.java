782ad64e63966a79985dabaeda557b07a0ccf811
lionel staricoff <staricofflionel@hotmail.com>
2024-02-18 01:34:19 -0500

    implementando la respuesta de las entidades al crear un usuario y al logearlo

--- BackEnd/homeservice/src/main/java/cohorte16/homeservice/controllers/UserController.java
+++ BackEnd/homeservice/src/main/java/cohorte16/homeservice/controllers/UserController.java
@@ -2,10 +2,16 @@ package cohorte16.homeservice.controllers;
 
 import cohorte16.homeservice.models.DatosLigin;
 import cohorte16.homeservice.models.DatosRegistroUsuario;
+import cohorte16.homeservice.models.User;
 import cohorte16.homeservice.services.impl.UserSeviceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -15,15 +21,33 @@ public class UserController {
     private UserSeviceImpl userSeviceImpl;
 
     @PostMapping
-    public void RegistrarUsuario(@RequestBody @Valid DatosRegistroUsuario datosRegistroUsuario){
-       userSeviceImpl.saveUser(datosRegistroUsuario).OrElse(userSeviceImpl.validateLogin(datosRegistroUsuario));
+    public ResponseEntity<?> RegistrarUsuario(@RequestBody @Valid DatosRegistroUsuario datosRegistroUsuario){
+
+        User userCreated;
+        try {
+          userCreated =  userSeviceImpl.saveUser(datosRegistroUsuario);
+
+        }catch (Exception ex){
+
+            return  ResponseEntity.status(HttpStatus.CONFLICT).body("El usuario ya existe");
+        }
+
+        //devulve por convencion la url con los datos del usuario crado
+        return ResponseEntity.created(URI.create("/usuarios/"+new DatosRegistroUsuario(userCreated).id())).body(DatosRegistroUsuario.builder()
+                .avatar(userCreated
+                        .getAvatar())
+                .email(userCreated.getEmail())
+                .id(userCreated.getId()).build());
     }
 
 
-    @GetMapping
-    public String login(@RequestBody @Valid DatosLigin datosLogin) {
-
-
-        return userSeviceImpl.validateLogin(datosLogin);
+    @PostMapping( value = "/login")
+    public ResponseEntity <String>login(@RequestBody @Valid DatosLigin datosLogin) {
+        try {
+            userSeviceImpl.validateLogin(datosLogin);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body("Inicio de secion correcto");
     }
 }
--- BackEnd/homeservice/src/main/java/cohorte16/homeservice/models/DatosRegistroUsuario.java
+++ BackEnd/homeservice/src/main/java/cohorte16/homeservice/models/DatosRegistroUsuario.java
@@ -3,6 +3,14 @@ package cohorte16.homeservice.models;
 
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
-public record DatosRegistroUsuario(@NotBlank String email, @NotBlank String contrasenia, String avatar) {
+@Builder
+public record DatosRegistroUsuario( Long id,@NotBlank String email, @NotBlank String contrasenia, String avatar) {
+    public DatosRegistroUsuario(User userCreated) {
+        this(userCreated.getId(),userCreated.getEmail(),userCreated.getContrasenia(),userCreated.getAvatar());
+    }
+
+
+
 }
