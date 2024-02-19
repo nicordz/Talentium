fdbd66fde33bb2cf88e34c243c078e5484cf781a
ariocal <126732667+ariocal@users.noreply.github.com>
2024-02-19 15:53:01 -0500

    Creacion de entidad Ordenes: repository y service

---
+++ BackEnd/homeservice/src/main/java/cohorte16/homeservice/models/Order.java
@@ -0,0 +1,47 @@
+package cohorte16.homeservice.models;
+
+import cohorte16.homeservice.enums.Orderstatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "ordenes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descripcion")
+    @NotBlank
+    private String description;
+
+    @Column(name = "precio")
+    @DecimalMin(value = "0.01")
+    private BigDecimal price;
+
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date date;
+
+    @Column(name = "ESTADO")
+    @Enumerated(value = EnumType.STRING)
+    private Orderstatus orderstatus;
+
+
+    @ManyToOne
+    @JoinColumn(name = "profesional_id", referencedColumnName = "id")
+    private Professional professional;
+
+}
