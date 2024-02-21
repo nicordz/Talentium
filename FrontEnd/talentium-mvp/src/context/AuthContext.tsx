d05630043b66736b5100d0cc1894d82c27bc6722
Brian Lagranda <brianlagranda@gmail.com>
2024-02-21 16:29:29 -0300

    Se implementó tipado de User

--- FrontEnd/talentium-mvp/src/context/AuthContext.tsx
+++ FrontEnd/talentium-mvp/src/context/AuthContext.tsx
@@ -1,67 +1,63 @@
 import React, {
-  createContext,
-  useContext,
-  useState,
-  ReactNode,
-  useCallback,
+    createContext,
+    useContext,
+    useState,
+    ReactNode,
+    useCallback,
 } from "react";
 
-interface User {
-  name: string;
-  email: string;
-  photoUrl: string;
-}
+import { User } from "../interfaces/User";
 
 interface AuthContextType {
-  user: User | null;
-  login: (user: User) => void;
-  logout: () => void;
+    user: User | null;
+    login: (user: User) => void;
+    logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuth must be used within a AuthProvider");
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuth must be used within a AuthProvider");
+    }
+    return context;
 };
 
 interface AuthProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const getUserFromLocalStorage = (): User | null => {
-  const storedUser = localStorage.getItem("user");
-  return storedUser ? JSON.parse(storedUser) : null;
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
 };
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(getUserFromLocalStorage());
+    const [user, setUser] = useState<User | null>(getUserFromLocalStorage());
 
-  const login = useCallback((user: User) => {
-    setUser(user);
-    saveUserToLocalStorage(user);
-  }, []);
+    const login = useCallback((user: User) => {
+        setUser(user);
+        saveUserToLocalStorage(user);
+    }, []);
 
-  const logout = useCallback(() => {
-    setUser(null);
-    clearUserFromLocalStorage();
-  }, []);
+    const logout = useCallback(() => {
+        setUser(null);
+        clearUserFromLocalStorage();
+    }, []);
 
-  const saveUserToLocalStorage = (user: User) => {
-    localStorage.setItem("user", JSON.stringify(user));
-  };
+    const saveUserToLocalStorage = (user: User) => {
+        localStorage.setItem("user", JSON.stringify(user));
+    };
 
-  const clearUserFromLocalStorage = () => {
-    localStorage.removeItem("user");
-  };
+    const clearUserFromLocalStorage = () => {
+        localStorage.removeItem("user");
+    };
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
