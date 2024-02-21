defabb63f48ef54b36605eb71a169dad2a3e7f2e
Brian Lagranda <brianlagranda@gmail.com>
2024-02-21 16:30:18 -0300

    Se modificó el getItem del localStorage de "mail" a "user"

--- FrontEnd/talentium-mvp/src/screens/LandingPage.tsx
+++ FrontEnd/talentium-mvp/src/screens/LandingPage.tsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from 'react';
-import DashboardCliente from './DashboardCliente';
-import Footer from './Footer';
-import Header from './Header';
-import Hero from './Hero';
-import Article from '../components/Article';
+import { useEffect, useState } from "react";import DashboardCliente from "./DashboardCliente";
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import Article from "../components/Article";
 
-const LandingPage = () => {
-  const [email, setEmail] = useState('');
+import { LoginForm } from "../interfaces/LoginForm";
 
-  useEffect(() => {
-    const storageEmail = localStorage.getItem('mail');
-    if (storageEmail) {
-      setEmail(JSON.parse(storageEmail));
-    }
-  }, []);
+const LandingPage: React.FC<LoginForm> = () => {
+    const [email, setEmail] = useState();
 
-  return (
-    <div className=" bg-gradient-to-b from-royal-blue-500  to-white
-    flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        {!email ? (
-          <section className=" body-font flex-grow">
-            <Hero />
-            <Article/>
-          </section>
-        ) : (
-          <>
-            <DashboardCliente />
-          </>
-        )}
-      </main>
-      
-      <Footer />
-    </div>
-  );
+    useEffect(() => {
+        const storageEmail = localStorage.getItem("user");
+        if (storageEmail) {
+            setEmail(JSON.parse(storageEmail));
+        }
+    }, []);
+
+    return (
+        <div
+            className=' bg-gradient-to-b from-royal-blue-500  to-white
+    flex flex-col min-h-screen'
+        >
+            <Header />
+            <main className='flex-grow'>
+                {!email ? (
+                    <section className='body-font flex-grow'>
+                        <Hero />
+                        <Article />
+                    </section>
+                ) : (
+                    <>
+                        <DashboardCliente />
+                    </>
+                )}
+            </main>
+
+            <Footer />
+        </div>
+    );
 };
 
 export default LandingPage;
