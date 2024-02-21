9e9c951b46efe0f931a7439639c1096aeed9a431
Brian Lagranda <brianlagranda@gmail.com>
2024-02-21 16:31:03 -0300

    Se modificó imagen usuario logeado

--- FrontEnd/talentium-mvp/src/screens/Header.tsx
+++ FrontEnd/talentium-mvp/src/screens/Header.tsx
@@ -1,67 +1,77 @@
-import React from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import React from "react";import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import NoAvatar from "../../public/NoAvatar.png?url";
 
 const Header: React.FC = () => {
-  const auth = useAuth();
-  const navigate = useNavigate();
+    const auth = useAuth();
+    const navigate = useNavigate();
 
-  const handleLogout = () => {
-    auth.logout();
-    navigate("/");
-  }
+    const handleLogout = () => {
+        auth.logout();
+        navigate("/");
+    };
 
-  return (
-    <header className=" body-font flex flex-row items-center justify-between">
-      <div className="pl-5 pt-2">
-        <Link to="/">
-          <img src="/Logo2.png" alt="" className="w-36 h-auto" />
-        </Link>
-      </div>
+    return (
+        <header className=' body-font flex flex-row items-center justify-between'>
+            <div className='pl-5 pt-2'>
+                <Link to='/'>
+                    <img src='/Logo2.png' alt='' className='w-36 h-auto' />
+                </Link>
+            </div>
 
-      <div className="">
-        <nav className="">
-          <Link
-            to=""
-            className="px-2 hover:bg-gradient-to-t from-royal-blue-800 rounded-md"
-          >
-            Cliente
-          </Link>
-          <Link
-            to=""
-            className="px-2 hover:bg-gradient-to-t from-royal-blue-800 rounded-md"
-          >
-            Profesional
-          </Link>
-        </nav>
-      </div>
+            <div className=''>
+                <nav className=''>
+                    <Link
+                        to=''
+                        className='px-2 hover:bg-gradient-to-t from-royal-blue-800 rounded-md'
+                    >
+                        Cliente
+                    </Link>
+                    <Link
+                        to=''
+                        className='px-2 hover:bg-gradient-to-t from-royal-blue-800 rounded-md'
+                    >
+                        Profesional
+                    </Link>
+                </nav>
+            </div>
 
-      <div className="pr-3">
-        {auth.user ? (
-          <>
-            <span className="py-1 px-3">{auth.user.name}</span>
-            <img src={auth.user.photoUrl} alt="Perfil" className="w-10 h-10 rounded-full" />
-            <button type="button" onClick={handleLogout}>Cerrar sesion</button>
-          </>
-        ) : (
-          <>
-            <NavLink
-          to="/Login"
-          className="py-1 px-3 focus:outline-none hover:bg-gradient-to-t from-royal-blue-800 rounded-md"
-        >
-          Ingresar
-        </NavLink>
-        <NavLink
-          to="/Register"
-          className="py-1 px-3 focus:outline-none hover:bg-gradient-to-t from-royal-blue-800 rounded-md"
-        >
-          Registrate
-        </NavLink>
-          </>
-        )}
-      </div>
-    </header>
-  );
+            <div className='pr-3 flex gap-2'>
+                {auth.user ? (
+                    <>
+                        <span className='py-1 px-3'>{auth.user.name}</span>
+                        <img
+                            src={
+                                auth.user.photoUrl
+                                    ? auth.user.photoUrl
+                                    : NoAvatar
+                            }
+                            alt='Perfil'
+                            className='w-8 h-8 rounded-full'
+                        />
+                        <button type='button' onClick={handleLogout}>
+                            Cerrar sesion
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <NavLink
+                            to='/Login'
+                            className='py-1 px-3 focus:outline-none hover:bg-gradient-to-t from-royal-blue-800 rounded-md'
+                        >
+                            Ingresar
+                        </NavLink>
+                        <NavLink
+                            to='/Register'
+                            className='py-1 px-3 focus:outline-none hover:bg-gradient-to-t from-royal-blue-800 rounded-md'
+                        >
+                            Registrate
+                        </NavLink>
+                    </>
+                )}
+            </div>
+        </header>
+    );
 };
 
 export default Header;
